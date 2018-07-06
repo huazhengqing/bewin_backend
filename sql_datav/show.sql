c54da5dafaaa99b46f6c4fec9f63b28f08608801
huazhengqing <36689326+huazhengqing@users.noreply.github.com>
2018-07-06 20:27:08 +0800

    sql

--- sql_datav/show.sql
+++ sql_datav/show.sql
@@ -1,6 +1,6 @@
 
 
-SELECT f_symbol,f_ex1_name,f_ex1_bid,FROM_UNIXTIME(f_ex1_ts/1000, '%T') as f_ex1_ts,f_ex1_fee,f_ex2_name,f_ex2_ask,FROM_UNIXTIME(f_ex2_ts/1000, '%T') as f_ex2_ts,f_ex2_fee,FROM_UNIXTIME(f_ts/1000, '%T') as f_ts,convert(f_spread, decimal(16, 8)) as f_spread,convert(f_fee, decimal(16, 8)) as f_fee,convert(f_profit, decimal(16, 8)) as f_profit,convert(f_profit_p, decimal(5, 3)) as f_profit_p
+SELECT f_symbol,f_ex1_name,f_ex1_bid,FROM_UNIXTIME(f_ex1_ts/1000, '%T') as f_ex1_ts,f_ex1_fee,f_ex2_name,f_ex2_ask,FROM_UNIXTIME(f_ex2_ts/1000, '%T') as f_ex2_ts,f_ex2_fee,FROM_UNIXTIME(f_ts/1000, '%T') as f_ts,convert(f_spread, decimal(16, 8)) as f_spread,convert(f_fee, decimal(16, 8)) as f_fee,convert(f_profit, decimal(16, 8)) as f_profit,FORMAT(convert(f_profit_p*100, decimal(5, 1)),1) as f_profit_p
 FROM db_bewin.t_spread_current 
 WHERE f_profit_p > 0 and f_ex1_bid > 0 and f_ex2_ask > 0
 ORDER BY f_ts desc, f_profit_p DESC 
